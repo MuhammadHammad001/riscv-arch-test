53e9fe52faa551436f142652f0d9f6c7be873506
MuhammadHammad001 <hammad.bashir@10xengineers.ai>
2024-10-24 16:40:26 +0500

    test run

--- riscof-plugins/rv32/sail_cSim/riscof_sail_cSim.py
+++ riscof-plugins/rv32/sail_cSim/riscof_sail_cSim.py
@@ -7,7 +7,6 @@ import logging
 import random
 import string
 from string import Template
-import distutils
 
 import riscof.utils as utils
 from riscof.pluginTemplate import pluginTemplate
@@ -20,24 +19,17 @@ class sail_cSim(pluginTemplate):
     __model__ = "sail_c_simulator"
     __version__ = "0.5.0"
 
-    docker_cmd = 'docker run -w /work -v{0}:/work -a stdout -a stderr --init {1} {2}'
-
     def __init__(self, *args, **kwargs):
         sclass = super().__init__(*args, **kwargs)
 
         config = kwargs.get('config')
         if config is None:
             logger.error("Config node for sail_cSim missing.")
-            raise SystemExit
+            raise SystemExit(1)
         self.num_jobs = str(config['jobs'] if 'jobs' in config else 1)
-        self.docker = bool(config['docker']) if 'docker' in config else False
-        self.docker_img = str(config['image']) if 'image' in config else 'riscv_compliance'
         self.pluginpath = os.path.abspath(config['pluginpath'])
-        path = config['PATH'] if 'PATH' in config else ""
-
-
-        self.sail_exe = { '32' : os.path.join(path,"riscv_sim_RV32"),
-                '64' : os.path.join(path,"riscv_sim_RV64")}
+        self.sail_exe = { '32' : os.path.join(config['PATH'] if 'PATH' in config else "","riscv_sim_RV32"),
+                '64' : os.path.join(config['PATH'] if 'PATH' in config else "","riscv_sim_RV64")}
         self.isa_spec = os.path.abspath(config['ispec']) if 'ispec' in config else ''
         self.platform_spec = os.path.abspath(config['pspec']) if 'ispec' in config else ''
         self.make = config['make'] if 'make' in config else 'make'
@@ -50,38 +42,21 @@ class sail_cSim(pluginTemplate):
         self.suite = suite
         self.work_dir = work_dir
         self.objdump_cmd = 'riscv{1}-unknown-elf-objdump -D {0} > {2};'
-        self.archtest_env = archtest_env
-        compile_cmd = 'riscv{1}-unknown-elf-gcc -march={0} \
-         -static -mcmodel=medany -fvisibility=hidden -nostdlib -nostartfiles'
-
-        if not self.docker:
-            compile_cmd += ' -T '+self.pluginpath+'/env/link.ld\
-                -I '+self.pluginpath+'/env/\
-                -I ' + archtest_env
-        else:
-            compile_cmd += ' -T /work/sail_work/env/link.ld\
-                -I /work/sail_work/env/\
-                -I /work/test/includes'
-        self.compile_cmd = compile_cmd
+        self.compile_cmd = 'riscv{1}-unknown-elf-gcc -march={0} \
+         -static -mcmodel=medany -fvisibility=hidden -nostdlib -nostartfiles\
+         -T '+self.pluginpath+'/env/link.ld\
+         -I '+self.pluginpath+'/env/\
+         -I ' + archtest_env
 
     def build(self, isa_yaml, platform_yaml):
         ispec = utils.load_yaml(isa_yaml)['hart0']
         self.xlen = ('64' if 64 in ispec['supported_xlen'] else '32')
         self.isa = 'rv' + self.xlen
-        if "64I" in ispec["ISA"]:
-            self.compile_cmd = self.compile_cmd+' -mabi='+'lp64 '
-        elif "32I" in ispec["ISA"]:
-            self.compile_cmd = self.compile_cmd+' -mabi='+'ilp32 '
-        elif "32E" in ispec["ISA"]:
-            self.compile_cmd = self.compile_cmd+' -mabi='+'ilp32e '
+        self.compile_cmd = self.compile_cmd+' -mabi='+('lp64 ' if 64 in ispec['supported_xlen'] else 'ilp32 ')
         if "I" in ispec["ISA"]:
             self.isa += 'i'
-        if "E" in ispec["ISA"]:
-            self.isa += 'e'
         if "M" in ispec["ISA"]:
             self.isa += 'm'
-        if "A" in ispec["ISA"]:
-            self.isa += 'a'
         if "C" in ispec["ISA"]:
             self.isa += 'c'
         if "F" in ispec["ISA"]:
@@ -89,28 +64,20 @@ class sail_cSim(pluginTemplate):
         if "D" in ispec["ISA"]:
             self.isa += 'd'
         objdump = "riscv{0}-unknown-elf-objdump".format(self.xlen)
-        if not self.docker:
-            if shutil.which(objdump) is None:
-                logger.error(objdump+": executable not found. Please check environment setup.")
-                raise SystemExit
-            compiler = "riscv{0}-unknown-elf-gcc".format(self.xlen)
-            if shutil.which(compiler) is None:
-                logger.error(compiler+": executable not found. Please check environment setup.")
-                raise SystemExit
-            if shutil.which(self.sail_exe[self.xlen]) is None:
-                logger.error(self.sail_exe[self.xlen]+ ": executable not found. Please check environment setup.")
-                raise SystemExit
-            if shutil.which(self.make) is None:
-                logger.error(self.make+": executable not found. Please check environment setup.")
-                raise SystemExit
-        else:
-            os.mkdir(os.path.join(self.work_dir,"sail_work"))
-            os.mkdir(os.path.join(self.work_dir,"test"))
-            self.test_dir = os.path.join(self.work_dir,"test")
-            distutils.dir_util.copy_tree(self.archtest_env,os.path.join(self.test_dir,"includes"))
-            distutils.dir_util.copy_tree(
-                    os.path.join(self.pluginpath,"env"),
-                    os.path.join(self.work_dir,"sail_work/env"))
+        if shutil.which(objdump) is None:
+            logger.error(objdump+": executable not found. Please check environment setup.")
+            raise SystemExit(1)
+        compiler = "riscv{0}-unknown-elf-gcc".format(self.xlen)
+        if shutil.which(compiler) is None:
+            logger.error(compiler+": executable not found. Please check environment setup.")
+            raise SystemExit(1)
+        if shutil.which(self.sail_exe[self.xlen]) is None:
+            logger.error(self.sail_exe[self.xlen]+ ": executable not found. Please check environment setup.")
+            raise SystemExit(1)
+        if shutil.which(self.make) is None:
+            logger.error(self.make+": executable not found. Please check environment setup.")
+            raise SystemExit(1)
+
 
     def runTests(self, testList, cgf_file=None, header_file= None):
         if os.path.exists(self.work_dir+ "/Makefile." + self.name[:-1]):
@@ -134,8 +101,9 @@ class sail_cSim(pluginTemplate):
             execute += self.objdump_cmd.format(elf, self.xlen, 'ref.disass')
             sig_file = os.path.join(test_dir, self.name[:-1] + ".signature")
 
-            execute += self.sail_exe[self.xlen] + '  -i -v --trace=step  --pmp-count=16 --pmp-grain=0  --test-signature={0} {1} > {2}.log 2>&1;'.format(sig_file, elf, test_name)
-
+            execute += self.sail_exe[self.xlen] + ' --enable-dirty-update -i -v --trace=step --pmp-count=16 --pmp-grain=0 --test-signature={0} {1} > {2}.log 2>&1;'.format(sig_file, elf, test_name)
+		# to enable pointer masking extension use --enable-zpm
+		# to enable the hardware pte update for d and a bit use --enable-dirty-update
             cov_str = ' '
             for label in testentry['coverage_labels']:
                 cov_str+=' -l '+label
@@ -152,7 +120,6 @@ class sail_cSim(pluginTemplate):
                 coverage_cmd = 'riscv_isac --verbose info coverage -d \
                         -t {0}.log --parser-name c_sail -o coverage.rpt  \
                         --sig-label begin_signature  end_signature \
-                        --test-label rvtest_code_begin rvtest_code_end \
                         -e ref.elf -c {1} -x{2} {3} {4} {5};'.format(\
                         test_name, ' -c '.join(cgf_file), self.xlen, cov_str, header_file_flag, cgf_mac)
             else:
